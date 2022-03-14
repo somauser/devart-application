1553f80afafc182f640d2b35b2b4136506aa3b49
Nantawat Laothong <nantawat.laothong001@umb.edu>
2022-03-13 23:36:28 -0400

    fixing masonry

--- server/public/js/main.js
+++ server/public/js/main.js
@@ -3,5 +3,11 @@ window.onload = () => {
     const grid = document.querySelector('.grid');
     const masonry = new Masonry(grid, {
         itemSelector: '.grid-item',
+        // columnWidth: 230,
+        fitWidth:true,
+    });
+    const masonry2 = new Masonry(grid, {
+        itemSelector: '.grid-item-show',
+        fitWidth:true,
     });
 }
